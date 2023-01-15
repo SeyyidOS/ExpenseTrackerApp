9611a032245604af4ad007b1fa8a77fd165ec74f
SeyyidOS <seyyidosmansevgili@gmail.com>
2023-01-15 09:07:58 +0300

    Finalized the project

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,24 +1,30 @@
 import { Pressable, View, Text, StyleSheet } from "react-native";
-import { GlobalStyles } from "../../constants/styles";
+import { useNavigation } from "@react-navigation/native";
 
-function ExpenseItem({ description, amount, date }) {
+import { GlobalStyles } from "../../constants/styles";
+import { getFormattedDate } from "../../util/date";
+
+function ExpenseItem({ id, description, amount, date }) {
+  const navigation = useNavigation();
+
+  function expensePressHandler() {
+    navigation.navigate("ManageExpense", { expenseId: id });
+  }
+
   return (
-    <Pressable>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItemStyle}>
         <View>
-          <Text style={[styles.textBase, styles.expenseItemStyle]}>
+          <Text style={[styles.textBase, styles.description]}>
             {description}
           </Text>
-          <Text style={styles.textBase}>
-            {date.getFullYear().toString() +
-              "-" +
-              date.getMonth().toString() +
-              "-" +
-              date.getDate().toString()}
-          </Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
-          <Text style={styles.amount}>{amount}</Text>
+          <Text style={styles.amount}>{amount.toFixed(2)}</Text>
         </View>
       </View>
     </Pressable>
@@ -28,6 +34,9 @@ function ExpenseItem({ description, amount, date }) {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
   expenseItemStyle: {
     padding: 12,
     marginVertical: 8,
@@ -56,6 +65,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 4,
+    minWidth: 80,
   },
   amount: {
     color: GlobalStyles.colors.primary500,
